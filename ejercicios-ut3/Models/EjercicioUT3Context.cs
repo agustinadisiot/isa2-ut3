12ac52d19f1c4298e94bd93ec7e76749f8da87a2
cpfontan <carina.fontan@fi365.ort.edu.uy>
2022-05-17 20:18:18 -0300

    Add Estudiante example

---
+++ ejercicios-ut3/Models/EjercicioUT3Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ejercicios_ut3.Models
+{
+    public class EjercicioUT3Context : DbContext
+    {
+        public EjercicioUT3Context(DbContextOptions<EjercicioUT3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Estudiante> Estudiantes { get; set; }
+    }
+}
+ 
---
+++ ejercicios-ut3/Models/Estudiante.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ejercicios_ut3.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int NroEstudiante { get; set; }
+    }
+}
