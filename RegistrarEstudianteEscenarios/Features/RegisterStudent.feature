d0a6fb10bd96289d5c439922bdb3a72163727b7e
Agustina <agustinadisiotvevo@gmail.com>
2022-05-19 10:00:42 -0300

    Adds spectflow proj

---
+++ RegistrarEstudianteEscenarios/Features/RegisterStudent.feature
@@ -0,0 +1,9 @@
+﻿Feature: Resgister Student
+
+@mytag
+Scenario: Register valid student
+	Given name "Agustina"
+	And lastname "Disiot"
+	And  number 221025
+	When create button in pressed
+	Then the user should be created
---
+++ RegistrarEstudianteEscenarios/Steps/ResgisterStudentSteps.cs
@@ -0,0 +1,49 @@
+﻿using ejercicios_ut3.Models;
+using System;
+using TechTalk.SpecFlow;
+
+namespace RegistrarEstudianteEscenarios.Features
+{
+    [Binding]
+    public sealed class ResgisterStudentSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private readonly Estudiante _registerStudent = new Estudiante();
+        private int _result;
+
+        public ResgisterStudentSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given(@"name ""(.*)""")]
+        public void GivenName(string name)
+        {
+            _registerStudent.Nombre = name;
+        }
+        
+        [Given(@"lastname ""(.*)""")]
+        public void GivenLastname(string lastname)
+        {
+            _registerStudent.Apellido = lastname;
+        }
+        
+        [Given(@"number (.*)")]
+        public void GivenNumber(int number)
+        {
+            _registerStudent.NroEstudiante = number;
+        }
+        
+        [When(@"create button in pressed")]
+        public void WhenCreateButtonInPressed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the user should be created")]
+        public void ThenTheUserShouldBeCreated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
